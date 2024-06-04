1856c4870aec1af45b8ef54f2fe342c0483a99c4
Indy <indhiraviana@gmail.com>
2024-06-04 16:02:22 -0300

    Desafio Dio Terminal Conta de Banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Todo: Conhecer e importar a classe Scanner
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        //Exibir as msgs para nosso usuario
+        System.out.println("Informe o número da sua Conta ");
+        int conta = scanner.nextInt();
+       
+        System.out.println("Informe o número da sua Agência com o dígito ");
+        String agencia = scanner.next();
+       
+        System.out.println("Informe o seu primeiro nome ");
+        String nome = scanner.next();
+       
+        System.out.println("Informe o saldo disponível na sua conta ");
+        double saldo = scanner.nextDouble();
+
+        //Obter pela classe Scanner os valores digitados no terminal
+
+        //Exibir a msg final
+        System.out.println("Olá " + nome + "! Obrigado por criar uma conta em nosso banco, sua Agência é: " + agencia + ", conta: " + conta + " e seu saldo de " + saldo + " já está disponível para saque.");
+
+        
     }
 }
